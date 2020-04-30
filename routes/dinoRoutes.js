4c46993e596a166d7ffa66745c6949bbb5907ef9
Stacey Eliuk <stacey.eliuk@gmail.com>
2020-04-30 19:54:04 -0400

    learning team lunch

--- routes/dinoRoutes.js
+++ routes/dinoRoutes.js
@@ -7,8 +7,12 @@ const { Dino } = require("../models/index");
 //GET all Dinosaurs
 router.get("/", async (req, res, next) => {
   try {
+    // retrieve our dinosaurs from our database - we don't have one set up,
+    // so this is really more theoretical
     const dinos = await Dino.findAll();
+    //if we successfully found the dinosaurs
     if (dinos) {
+      //we send back the response
       res.status(200).json(dinos);
     } else {
       res.send("not found").status(404);
